54212a5fe6386ff9c279c177b9a7401600a3f86b
Andrey Chilikin <achilikin@gmail.com>
2014-12-05 19:48:25 +0000

    Adopted for common API

--- rht03.c
+++ rht03.c
@@ -1,11 +1,11 @@
-/*	Polling of RHT03 relative humidity/temperature sensor 
-	Does not use floating point math to save space on ATtiny85
-	Note: should work with DHT-22 as well
+/*  Reading RHT03 relative humidity/temperature sensor 
+    Does not use floating point math to save space on ATtiny85
+    Note: should work with DHT-22 as well
 
-	This copy is optimized for AVR Atmega32 on MMR-70
-	Copyright (c) 2014 Andrey Chilikin (https://github.com/achilikin)
+    This copy is optimized for AVR Atmega32 on MMR-70
+    Copyright (c) 2014 Andrey Chilikin (https://github.com/achilikin)
     
-	This program is free software: you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -18,30 +18,43 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <stdio.h>
 #include <string.h>
 #include <avr/io.h>
 #include <avr/eeprom.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#include "rht.h"
 #include "rht03.h"
+#include "timer.h"
 #include "mmr70pin.h"
 
-uint16_t EEMEM em_rht_errors = 0; // number of poll errors
+#define RHTDATA     46 // start bit, 40 bits raw data, 5 decoded data
 
+typedef struct rht03_s
+{
+	uint8_t  valid;   // data[] is valid
+	uint8_t  data[5]; // 40 bits read from RHT03
+	uint16_t errors;  // number of polling errors for debugging
+	uint8_t  bits[RHTDATA]; // bit array read from RHT03
+} rht03_t;
+
+static rht03_t rht03;
+static uint16_t EEMEM em_rht_errors = 0; // number of poll errors
 static const uint8_t MAX_COUNTER = 0xFF;
 
-void rht_init(rht03_t *prht)
+void rht03_init(void)
 {
-	prht->valid = 0;
-	prht->errors = eeprom_read_word(&em_rht_errors);
-	memset(prht->data, 0, 5);
+	rht03.valid = 0;
+	rht03.errors = eeprom_read_word(&em_rht_errors);
+	memset(rht03.data, 0, 5);
 	mmr_tp5_mode(INPUT_HIGHZ);
 }
 
-int8_t rht_poll(rht03_t *prht)
+int8_t rht03_poll(rht_t *prht)
 {
-	uint8_t *bits = prht->bits;
+	uint8_t *bits = rht03.bits;
     uint8_t nbit = 0;
     uint8_t counter;
     uint8_t j = 0, i;
@@ -88,54 +101,68 @@ int8_t rht_poll(rht03_t *prht)
 
     // if the checksum matches mark data valid and return true
     if ((uint8_t)(pdata[0] + pdata[1] + pdata[2] + pdata[3]) == pdata[4]) {
-		memcpy(prht->data, pdata, 5);
-		prht->valid = 1;
+		memcpy(rht03.data, pdata, 5);
+		rht03.valid = RHT_TVALID | RHT_HVALID;
         return 0;
     }
 	
-	prht->errors ++;
-	eeprom_update_word(&em_rht_errors, prht->errors);
-    
+	rht03.errors++;
+	eeprom_update_word(&em_rht_errors, rht03.errors);
+    prht->errors = rht03.errors;
 	return -1;
 }
 
 // parse data bits and return temperature in centigrades
-// if decimal is not NULL store decimal there
-int8_t rht_getTemperature(rht03_t *prht, uint8_t *decimal)
+int8_t rht03_getTemperature(rht_t *prht)
 {
     int16_t temperature; 
 	
-	if (!prht->valid)
-		return RHT_ERROR;
+	if (!rht03.valid)
+		return -1;
     
-	temperature = (int16_t)(prht->data[2] & 0x7F);
+	temperature = (int16_t)(rht03.data[2] & 0x7F);
     temperature <<= 8;
-    temperature |= prht->data[3];
-    if (decimal)
-        *decimal = temperature % 10;
+    temperature |= rht03.data[3];
+	prht->temperature.dec = temperature % 10;
     temperature /= 10;
-    if (prht->data[2] & 0x80)
+    if (rht03.data[2] & 0x80)
         temperature *= -1;
+	prht->temperature.val = temperature;
+	prht->valid |= RHT_TVALID;
 
-	return temperature;
+	return 0;
 }
 
 // parse data bits and return relative humidity in %
-// if decimal is not NULL store decimal there
-int8_t rht_getHumidity(rht03_t *prht, uint8_t *decimal)
+int8_t rht03_getHumidity(rht_t *prht)
 { 
     uint16_t humidity;
 
-	if (!prht->valid)
-		return RHT_ERROR;
+	if (!rht03.valid)
+		return -1;
 
-    humidity = prht->data[0];
+    humidity = rht03.data[0];
     humidity <<= 8;
-    humidity |= prht->data[1];
-    if (decimal)
-        *decimal = humidity % 10;
+    humidity |= rht03.data[1];
+	prht->humidity.dec = humidity % 10;
     humidity /= 10;
+	prht->humidity.val = humidity;
+	prht->valid |= RHT_HVALID;
 
-    return humidity;
+    return 0;
 }
 
+void rht03_print(const char *data)
+{
+	const uint8_t *buf = rht03.bits;
+	printf("%u %lu rht %s: ", rht03.errors, millis(), data ? "error" : "ok");
+	for(uint8_t i = 0; i < 41; i++)
+		printf("%u ", buf[i]);
+	printf("| ");
+	for(uint8_t i = 0; i < 5; i++)
+		printf("%02x ", buf[i+41]);
+	printf("| ");
+	if (data != NULL)
+		printf("%s", data);
+	printf("\n");
+}
--- rht03.h
+++ rht03.h
@@ -1,11 +1,11 @@
-/*	Polling of RHT03 relative humidity/temperature sensor 
-	Does not use floating point math to save space on ATtiny85
-	Note: should work with DHT-22 as well
+/*  Reading RHT03 relative humidity/temperature sensor 
+    Does not use floating point math to save space on ATtiny85
+    Note: should work with DHT-22 as well
 
-	This copy is optimized for AVR Atmega32 on MMR-70
-	Copyright (c) 2014 Andrey Chilikin (https://github.com/achilikin)
+    This copy is optimized for AVR Atmega32 on MMR-70
+    Copyright (c) 2014 Andrey Chilikin (https://github.com/achilikin)
     
-	This program is free software: you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -19,8 +19,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _RHT03_MMR_70_H_
-#define _RHT03_MMR_70_H_
+#ifndef __RHT03_MMR_70_H__
+#define __RHT03_MMR_70_H__
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -32,21 +32,11 @@ extern "C" {
 #endif
 #endif
 
-#define RHTDATA     46 // start bit, 40 bits raw data, 5 decoded data
-#define RHT_ERROR -120 // valid readings for humidity 0:100, temperature -40:+80
-
-typedef struct rht03
-{
-	uint8_t  valid;   // data[] is valid
-	uint8_t  data[5]; // 40 bits read from RHT03
-	uint16_t errors;  // number of polling errors for debugging
-	uint8_t  bits[RHTDATA]; // bit array read from RHT03
-}rht03_t;
-
-void rht_init(rht03_t *prht);
-int8_t rht_poll(rht03_t *prht);
-int8_t rht_getTemperature(rht03_t *prht, uint8_t *decimal);
-int8_t rht_getHumidity(rht03_t *prht, uint8_t *decimal);
+void   rht03_init(void);
+void   rht03_print(const char *data);
+int8_t rht03_poll(rht_t *prht);
+int8_t rht03_getTemperature(rht_t *prht);
+int8_t rht03_getHumidity(rht_t *prht);
 
 #ifdef __cplusplus
 }
